e365bade951ef269aa536d2ff5757a5b0350b7b2
giorgosdip <gdiplarosx@gmail.com>
2023-04-25 22:14:06 +0300

    nonos new

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 		document.write('<link href="./css/style.css?r=' + Math.floor(Math.random() * 100) + '" rel="stylesheet" type="text/css" />');
 	</script>
 	<script src="./script.js"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -20,6 +23,7 @@
 
 
 	$length = count($data);
+	echo "<div class='nonos'>";
 	for ($i = 0; $i < $length; $i++) {
 		echo "
 	<div class='widget-container list-item-1-container'>
@@ -34,7 +38,8 @@
 
 	</div>";
 
-	} ?>
+	}
+	"</div>" ?>
 
 
 </body>
