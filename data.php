612d0796a8a76c08ac20e8c0a207dde9f44268ae
Panagiotis Gazis <panagiwthsgazhs@gmail.com>
2023-04-25 21:16:08 +0300

    update_php_3

--- data.php
+++ data.php
@@ -5,13 +5,13 @@ $data = [
             'img' => 'https://mediterraneangold.gr/wp-content/uploads/2021/06/%CE%A1%CE%99%CE%93%CE%91%CE%9D%CE%97-%CE%97-%CE%95%CE%9B%CE%9B%CE%97%CE%9D%CE%99%CE%9A%CE%97.jpg',
             'title' => 'Ρίγανη Τριμμένη',
             'price' => 10.99,
-            'keimeno' => 'keimenaki 122112'
+            'keimeno' => 'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
         ],
         'gr' => [
             'img' => 'https://m.media-amazon.com/images/I/81jPNHaPN1L._SX522_.jpg',
             'title' => 'Ρίγανη Τριμμένη',
             'price' => 10.99,
-            'keimeno' =>'keimenaki 122112'
+            'keimeno' =>'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
         ]
     ],
     [
@@ -19,13 +19,13 @@ $data = [
             'img' => 'https://mediterraneangold.gr/wp-content/uploads/2021/06/%CE%A1%CE%99%CE%93%CE%91%CE%9D%CE%97-%CE%97-%CE%95%CE%9B%CE%9B%CE%97%CE%9D%CE%99%CE%9A%CE%97.jpg',
             'title' => 'Ρίγανη Χύμα',
             'price' => 12.99,
-            'keimeno' =>'keimenaki 122112'
+            'keimeno' =>'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
         ],
         'gr' => [
             'img' => 'https://cdn.shopify.com/s/files/1/0994/9584/products/sweet_tea_leaves-2_600x.jpg?v=1536186114',
             'title' => 'Ρίγανη Χύμα',
             'price' => 12.99,
-            'keimeno' =>'keimenaki 122112'
+            'keimeno' =>'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
         ]
         ],
         [
@@ -33,27 +33,27 @@ $data = [
                 'img' => 'https://cdn.shopify.com/s/files/1/0994/9584/products/sweet_tea_leaves-2_600x.jpg?v=1536186114',
                 'title' => 'Τσάι Τριμμένο',
                 'price' => 10.99,
-                'keimeno' => 'keimenaki 122112'
+                'keimeno' => 'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
             ],
             'gr' => [
                 'img' => 'https://cdn.shopify.com/s/files/1/0994/9584/products/sweet_tea_leaves-2_600x.jpg?v=1536186114',
-                'title' => 'Τσάι Τριμμένο)',
+                'title' => 'Τσάι Τριμμένο',
                 'price' => 10.99,
-                'keimeno' =>'keimenaki 122112'
+                'keimeno' =>'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
             ]
         ],
         [
             'en' => [
                 'img' => 'https://cdn.shopify.com/s/files/1/0994/9584/products/sweet_tea_leaves-2_600x.jpg?v=1536186114',
-                'title' => 'Τσάι Χύμα)',
+                'title' => 'Τσάι Χύμα',
                 'price' => 12.99,
-                'keimeno' => 'keimenaki 122112'
+                'keimeno' => 'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
             ],
             'gr' => [
                 'img' => 'https://cdn.shopify.com/s/files/1/0994/9584/products/sweet_tea_leaves-2_600x.jpg?v=1536186114',
                 'title' => 'Τσάι Χύμα',
                 'price' => 12.99,
-                'keimeno' => 'keimenaki 122112'
+                'keimeno' => 'The glowing formula in our women’s body wash works with skin long after you shower to reveal natural radiance.'
             ]
         ]
 ];
