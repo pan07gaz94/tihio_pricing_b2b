fd8ae71a39bda6079a596728b6722560b3f79813
Panagiotis Gazis <panagiwthsgazhs@gmail.com>
2023-04-30 22:27:07 +0300

    Add Favicons
    Add Favicons

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 <html lang="en">
 
 <head>
+	<link rel="shortcut icon" type="image/x-icon" href="./iconified/favicon1.ico">
+	<link rel="shortcut icon" href="./iconified/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="./iconified/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="./iconified/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="./iconified/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="./iconified/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="./iconified/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="./iconified/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="./iconified/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="./iconified/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="./iconified/apple-touch-icon-180x180.png" />
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-MM23BV4M4R"></script>
 	<script>
@@ -14,6 +25,9 @@
 
 		gtag('config', 'G-MM23BV4M4R');
 	</script>
+
+
+
 
 
 	<meta name="robots" content="noindex, nofollow">
